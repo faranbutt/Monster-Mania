36fb379b4b394bd6da26eabd92beadbcdd57eda2
faranbutt <faranbutt789@gmail.com>
2023-02-23 13:04:10 +0500

    Fixing App

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,11 @@
-import { Component } from "react";
 import Card from "../Card/card.component";
-class CardList extends Component {
-    
-    render(){
-        console.log('CardList render')
-        const {monster} = this.props;
-        return(
-            <div className="w-2/3 grid grid-cols-4 gap-5 mt-0 mx-auto">
-                {monster.map((monster)=>{
-                    return (
-                       <Card key={monster.id} monster={monster}/>
-                    );
-                })}
-            </div>
-        );
-    }
-}
+const CardList = ({ monster }) => {
+  return (
+    <div className="w-2/3 grid grid-cols-4 gap-5 mt-0 mx-auto">
+      {monster.map((monster) => {
+        return <Card key={monster.id} monster={monster} />;
+      })}
+    </div>
+  );
+};
 export default CardList;
--- src/components/searchbox/searchbox.component.jsx
+++ src/components/searchbox/searchbox.component.jsx
@@ -1,12 +1,13 @@
-import { Component } from "react";
-
-class SearchBox extends Component{
-    render(){
-        return(
-            <div className="flex justify-center">
-            <input className={this.props.className} type='search' placeholder='search-monsters' onChange={this.props.onChangeHandler} />
-            </div>
-        );
-    };
-}
+const SearchBox = ({ className, onChangeHandler }) => {
+  return (
+    <div className="flex justify-center">
+      <input
+        className={className}
+        type="search"
+        placeholder="search-monsters"
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+};
 export default SearchBox;
